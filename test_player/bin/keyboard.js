7e925eb7b6aeb5f15537ad12bbf3ae8ae321883f
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-06-10 03:04:01 +0600

    Update keyboard.js
    help text corrected

--- test_player/bin/keyboard.js
+++ test_player/bin/keyboard.js
@@ -6,13 +6,13 @@ export class Keyboard {
     help() {
         return `
         Управление в игре:
-         - вперёд:      ${this.controls.up}
-         - назад:       ${this.controls.down}
-         - влево:       ${this.controls.left}
-         - вправо:      ${this.controls.right}
-         - выбор юнита: ${this.controls.switch_unit}
-         - поймать:     ${this.controls.catch}
-         - бросок:      ${this.controls.throw}
+         - вперёд:.........${this.controls.up}
+         - назад:..........${this.controls.down}
+         - влево:..........${this.controls.left}
+         - вправо:.........${this.controls.right}
+         - выбор юнита:....${this.controls.switch_unit}
+         - поймать:........${this.controls.catch}
+         - бросок:.........${this.controls.throw}
         `;
     }
 }
