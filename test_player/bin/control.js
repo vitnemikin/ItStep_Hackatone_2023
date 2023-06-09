92d3c7843c2010875217e8c5e5845725817139b6
vitnemikin <49492880+vitnemikin@users.noreply.github.com>
2023-06-10 05:42:38 +0600

    Key listener added

--- test_player/bin/control.js
+++ test_player/bin/control.js
@@ -1,10 +1,12 @@
+import keypress from "keypress";
+
 export class Control {
-    listening = false;
+    #listener = null;
     action = {unit:0, switch:0, type:"", angle:0, force:0};
 
     constructor(controls) {
         this.controls = controls;
-        
+        keypress(process.stdin);
     }
 
     help() {
@@ -36,14 +38,64 @@ export class Control {
     }
 
     startListenKeys() {
-        if (this.listening) return;
-        this.listening = true;
+        if (this.#listener) return;
+        this.#listener = this.#keyListener.bind(this);
+        process.stdin.setRawMode(true);
+        process.stdin.on("keypress", this.#listener);
+        process.stdin.resume();
         
     }
 
     stopListenKeys() {
-        if (!this.listening) return;
-        this.listening = false;
+        if (!this.#listener) return;
+        process.stdin.setRawMode(false);
+        process.stdin.off("keypress", this.#listener);
+        process.stdin.pause();
 
+    }
+
+    #keyListener(char, key) {
+        switch(key.name) {
+            case this.controls.switch_unit:
+                this.action.switch = true;
+                break;
+
+            case this.controls.catch:
+                this.action.type = "catch";
+                break;
+
+            case this.controls.throw:
+                this.action.type = "throw";
+                this.action.force = 100;
+                break;
+
+            case this.controls.up:
+                this.action.type = "run";
+                this.action.force = 100;
+                this.action.angle = 0;
+                break;
+
+            case this.controls.down:
+                this.action.type = "run";
+                this.action.force = 100;
+                this.action.angle = 180;
+                break;
+
+            case this.controls.left:
+                this.action.type = "run";
+                this.action.force = 100;
+                this.action.angle = 90;
+                break;
+            
+            case this.controls.right:
+                this.action.type = "run";
+                this.action.force = 100;
+                this.action.angle = 270;
+                break;
+    
+            case "escape":
+                this.stopListenKeys();
+                break;
+        }
     }
 }
